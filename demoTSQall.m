6b7c7b5febb328404cd80a2b09e56d22298aeb2d
taoshiqian <17210240200@fudan.edu.cn>
2018-06-14 13:52:29 +0800

    修改主代码

--- demoTSQall.m
+++ demoTSQall.m
@@ -1,12 +1,15 @@
 %startup
+% 测试所有颜色空间下，“暗光区域增量”+“过曝光区域降光”双重功能下的结果，保存在allColor文件夹下。
+% allColor文件夹中，P（或Q属于输入图片的名字）。其他图的名字含义是:图片名_颜色空间_操作.后缀。
+% 其中操作包括Dark(过曝光区域降光)  Light(暗光区域增量)    Light_Dark(“暗光区域增量”+“过曝光区域降光”)
 addpath ours;
 addpath colorspace;
 
 cs = {'YCbCr','JPEG-YCbCr','YDbDr','YPbPr','YUV','YIQ','HSV','HSL','HSI','Lab','Luv','LCH',};% 'XYZ','CAT02 LMS','RGB'
 idx= {  1,        1,          1,     1,     1,    1,     3,    3,    3,    1,    1,    1,  };%   0   ,    0   ,    0
 
-file = 'D:\MATLAB\HDR\OpenCE\allColor\Q';%P1010520
-I = imread( [file,'.jpg'] ); 
+file = 'D:\MATLAB\HDR\OpenCE\allColor\Q';%修改输入图片的路径
+I = imread( [file,'.jpg'] ); %修改输入图片的后缀
 
 
 for ii = 1:numel(cs)
--- demoTSQtest.m
+++ demoTSQtest.m
@@ -1,6 +1,6 @@
 %评价指标
 addpath quality
-raw = imread('D:\MATLAB\HDR\BIMEF\dataset\01.jpg');
-enhanceResult = imread('D:\MATLAB\HDR\BIMEF\dataset\01_out.png');
+raw = imread('D:\MATLAB\HDR\BIMEF\dataset\01.jpg'); %输入图像
+enhanceResult = imread('D:\MATLAB\HDR\BIMEF\dataset\01_out.png'); %增强结果图
 loe100x100(raw,enhanceResult)
 vif(enhanceResult,enhanceResult)
